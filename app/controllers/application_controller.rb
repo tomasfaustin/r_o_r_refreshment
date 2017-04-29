63075e860ceb687e3fee4ba588681f4147670e47
Thomas Huisking <tomasfaustin@gmail.com>
2017-04-28 17:08:29 -0700

    Update app_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def goodbye
-  render html: "¡Goodbye, world!"
+  render html: "¡Hola, mundo!"
   end
 
 end
